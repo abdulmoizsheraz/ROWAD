0969c7d29ec0a576cffe66c9a3576f39d4e87275
moizsheraz <moiz77131@gmail.com>
2024-05-07 20:16:46 +0500

    feat:UI

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -6,10 +6,13 @@ const sendToken = (user, statusCode, res) => {
   // options for cookie
   const options = {
     expires: new Date(
-      Date.now() + 7 * 24 * 60 * 60 * 1000
+        Date.now() + 60 * 24 * 60 * 60 * 1000 // Token expiration time (in milliseconds)
     ),
     httpOnly: true,
-  };
+    secure: true,             // Ensure the cookie is sent only over HTTPS (recommended for production)
+    sameSite: "strict",       // Restrict cookie to same-site requests only
+    path: "/",                // Path where the cookie is accessible (root path)
+}
 
   res.status(statusCode).cookie("token", token, options).json({
     success: true,
